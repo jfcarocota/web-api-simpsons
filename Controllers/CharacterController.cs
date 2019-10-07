91279c289f08d6427f403dee132cf467aa55b809
jfcarocota <jfcarocota@gmail.com>
2019-10-07 09:40:27 -0700

    publish api

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using web_api_simpsons.Modules;
+using web_api_simpsons.Dependencies;
+
+namespace web_api_simpsons.Controllers
+{
+    [Route("simpsons/[controller]")]
+    [ApiController]
+    public class CharacterController : ICharacter
+    {
+        List<Character> listOfCharacters => new List<Character>
+        {
+            new Character
+            {
+                FirstName = "Homero",
+                SecondName = "Jay",
+                LastName = "Simpsons",
+                Age = 34
+            },
+            new Character
+            {
+                FirstName = "Bartolomeo",
+                SecondName = " ",
+                LastName = "Simpsons",
+                Age = 10
+            }
+        };
+        
+        [HttpGet("{id}")]
+        public Character GetCharacter(int id)
+        {
+            return listOfCharacters[id];
+        }
+
+        [HttpGet]
+        public List<Character> GetCharacterList()
+        {
+            return listOfCharacters;
+        }
+    }
+}
